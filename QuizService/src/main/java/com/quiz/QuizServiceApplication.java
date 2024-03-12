37b7c32a80091452d69ef1bf64c54f946ee199e7
“Sagar_Pachode3” <sagarpachode3@gmail.com>
2024-03-12 19:35:36 +0530

    Created ServiceRegistry for both the services. Worked on Load Balencing. Used Eureka dependancy. Working on API Gateway.

--- QuizService/src/main/java/com/quiz/QuizServiceApplication.java
+++ QuizService/src/main/java/com/quiz/QuizServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-public class QuizServiceApplication {
+public class QuizServiceApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuizServiceApplication.class, args);
